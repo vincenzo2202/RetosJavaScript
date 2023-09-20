55caf89a929959064e8d6c1e5277161d5b2dcd36
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 16:39:54 +0200

    Reto3 mostrar en console un string solicitado con prompt

---
+++ Reto3/js/main.js
@@ -0,0 +1,7 @@
+// 3. Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir
+// (recuerda usar prompt).
+
+let nombre = prompt();
+
+console.log(`Hola ${nombre}`);
+    
