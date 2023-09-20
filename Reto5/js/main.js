cc4afdd08688b646d2e0a55cf58da99c733e412f
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 17:32:27 +0200

    Reto 5 js calculadora si un numero es divisible entre 2 por consola

---
+++ Reto5/js/main.js
@@ -0,0 +1,14 @@
+// 5. Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
+// debemos indicarlo.
+
+ let a = prompt();
+ let b = 2 
+
+ if(a%b==0) {
+    console.log(`Es divisible entre 2`);
+ } else{
+    console.log(`No es divisible entre 2`)
+ }
+
+
+
