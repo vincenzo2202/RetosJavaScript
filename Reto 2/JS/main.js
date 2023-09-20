a636c28593c6960b5b7db8919086bedc80ed2bb3
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 16:32:00 +0200

    Reto 2 declarar string y mostrar por consola

---
+++ Reto 2/JS/main.js
@@ -0,0 +1,8 @@
+// 2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+// por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+
+let nombre = "Vincenzo";
+
+console.log(`Hola ${nombre}`);
+    
