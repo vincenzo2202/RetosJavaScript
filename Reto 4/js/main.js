02359b3aa1bdd2837be48bd5de0ece3066ee57cc
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 17:21:22 +0200

    Reto 4 calcular Área de un círculo

---
+++ Reto 4/js/main.js
@@ -0,0 +1,11 @@
+// 4. Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá por teclado
+// (recuerda pasar de String a double con parseDouble). Usa la constante PI.
+
+const pi=3.1416;4
+let radio = prompt();
+let area;
+
+area = pi*(radio**2);
+
+console.log(parseFloat(area));
+
