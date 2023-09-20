56d64fa9d6ec651303c087685b553215ce34857c
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 17:38:38 +0200

    Reto 6 Calcular el precio final con iva a 21% por consola

---
+++ Reto6/js/main.js
@@ -0,0 +1,16 @@
+// 6. Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
+// calcule el precio final con IVA. El IVA será una constante que sera del 21%
+
+ let a = prompt();
+ const iva = 1.21;
+ let precio
+
+ precio=a*iva;
+
+ console.log(parseFloat(precio));
+
+ 
+ 
+
+
+
