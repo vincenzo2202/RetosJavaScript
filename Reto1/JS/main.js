f2a8e1200821b00791d890e85330c2823f1c038c
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-20 16:22:04 +0200

    Reto 1 JS número mayor

---
+++ Reto1/JS/main.js
@@ -0,0 +1,16 @@
+/*
+1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+funciona.
+
+*/
+
+let a = 20;
+let b = 15;
+
+if (a>b){
+    console.log(`El número mayor es ${a}`)
+
+} else {
+    console.log(`El número mayor es ${b}`)
+}
